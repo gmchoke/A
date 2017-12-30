1915a97aa542bdbc1804d145b31da503e8b9fdf2
gmchoke <34873600+gmchoke@users.noreply.github.com>
2017-12-31 00:49:55 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Menu
+
+echo -e "* 1         : ดูรายการต่างมีดังนี้"
+echo -e "* 2         : สร้างชื่อผู้ใช้งานและรหัสผ่าน SSH & OpenVPN"
+echo -e "* trial     : membuat akun trial"
+echo -e "* hapus     : menghapus akun SSH & OpenVPN"
+echo -e "* cek       : cek user login"
+echo -e "* member    : daftar member SSH & OpenVPN"
+echo -e "* resvis    : restart service dropbear, webmin"
+echo -e "              squid3, OpenVPN dan SSH"
+echo -e "* reboot    : reboot VPS"
+echo -e "* speedtest : ทดสอบเทสสปีด VPS"
+echo -e "* info      : menampilkan informasi sistem"
+echo -e "* about     : info script auto install"
+echo -e "* exit      : keluar dari Putty/Connecbot/"
+echo -e "              JuiceSSH"
+echo -e ""
