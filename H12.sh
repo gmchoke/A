37fba97c409ba1f6eb35d06dae607c4acf157219
gmchoke <34873600+gmchoke@users.noreply.github.com>
2018-01-03 11:16:26 +0700

    Update and rename H1.sh to H12.sh

--- H12.sh
+++ H12.sh
@@ -65,9 +65,9 @@ cp client.ovpn /home/vps/public_html/
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://scripkguza.000webhostapp.com/KGUZA-ALL-SCRIP/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://scripkguza.000webhostapp.com/KGUZA-ALL-SCRIP/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
