5c0a0b4ffd3ec19e3f50ca896b834776e0d11a35
gmchoke <34873600+gmchoke@users.noreply.github.com>
2018-01-03 10:59:24 +0700

    Update and rename HO1.sh to H1.sh

--- H1.sh
+++ H1.sh
@@ -53,7 +53,7 @@ sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/D/master/iptables
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/D/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
