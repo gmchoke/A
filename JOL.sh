48300bd0be22991dcc73bd3feed39543047d4c5b
gmchoke <34873600+gmchoke@users.noreply.github.com>
2018-01-03 10:41:42 +0700

    Update JOL.sh

--- JOL.sh
+++ JOL.sh
@@ -30,7 +30,7 @@ apt-get update
 apt-get install neofetch 
 echo "clear" >> .bashrc 
 echo 'echo -e "Selamat datang di server $HOSTNAME"' >> .bashrc
-echo 'echo -e "Script mod by Bustami Arifin"' >> .bashrc
+echo 'echo -e "Script mod by GM CHOKE"' >> .bashrc
 echo 'echo -e "Ketik menu untuk menampilkan daftar perintah"' >> .bashrc
 echo 'echo -e ""' >> .bashrc
 
@@ -47,13 +47,13 @@ service nginx restart
 wget -O /etc/openvpn/openvpn.tar "https://scripkguza.000webhostapp.com/KGUZA-ALL-SCRIP/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/gmchoke/A/master/1194.conf"
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/gmchoke/D/master/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/A/master/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/D/master/iptables
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
