463008006b251ab76e7c8d8bc23da4bbf671fa07
gmchoke <34873600+gmchoke@users.noreply.github.com>
2018-03-21 09:09:08 +0700

    Update webmin-ubuntu14.04.sh

--- webmin-ubuntu14.04.sh
+++ webmin-ubuntu14.04.sh
@@ -21,7 +21,7 @@ wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/gmchoke/GMCHOK
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-iptables -t nat -I POSTROUTING -s 1.1.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
 wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/A/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
