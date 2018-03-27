8f31834376dcefb6c08e63ef03e4101d2f6b34dc
gmchoke <34873600+gmchoke@users.noreply.github.com>
2018-03-27 21:20:29 +0700

    Update and rename webmin-ubuntu14.04.sh to Ubun14.sh

--- Ubun14.sh
+++ Ubun14.sh
@@ -21,9 +21,9 @@ wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/gmchoke/GMCHOK
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-iptables -t nat -I POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/A/master/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/new1/master/iptables
 chmod +x /etc/network/if-up.d/iptables
 cp /usr/lib/openvpn/openvpn-plugin-auth-pam.so /etc/openvpn/
 mkdir /etc/openvpn/tmp
@@ -40,7 +40,7 @@ chmod +x usernew
 # Install Squid
 apt-get -y install squid3
 cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/gmchoke/A/master/squid.conf"
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/gmchoke/new1/master/squid.conf"
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
 service squid3 restart
@@ -75,7 +75,6 @@ sed -i s/nl/th/g /var/www/html/vnstat/config.php;
 wget -O /var/www/html/vnstat/lang/th.php "https://raw.githubusercontent.com/gmchoke/A/master/th.php"
 wget -O /var/www/html/vnstat/index.php "https://raw.githubusercontent.com/gmchoke/A/master/index.php"
 sed -i s/xxxxxxxxxx/http/g /var/www/html/vnstat/index.php;
-wget -O /var/www/html/vnstat/openvpn-as.png "https://docs.google.com/uc?export=download&id=1cmgyFpofMxFMQApLf2G4C7woQCc032rf"
 wget -O /etc/apache2/sites-enabled/000-default.conf "https://raw.githubusercontent.com/gmchoke/A/master/000-default.conf"
 sed -i s/85/80/g /etc/apache2/sites-enabled/000-default.conf;
 sed -i s/85/10000/g /var/www/html/vnstat/index.php;
