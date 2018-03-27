822266539ec2498861e85a0095733603b31966d8
gmchoke <34873600+gmchoke@users.noreply.github.com>
2018-03-27 21:59:58 +0700

    Update Ubun14.sh

--- Ubun14.sh
+++ Ubun14.sh
@@ -20,7 +20,7 @@ sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/new1/master/iptables
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gmchoke/new1/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
 cp /usr/lib/openvpn/openvpn-plugin-auth-pam.so /etc/openvpn/
 mkdir /etc/openvpn/tmp
